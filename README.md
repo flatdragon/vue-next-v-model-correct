c255e1d92882053a094d697a22083c9427781722
Kochetygov Andrew <flatdragon@yandex.ru>
2020-07-06 13:51:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Vue 2 correct v-model behaviour with UNDEFINED values
+## See behaviour on [GitHub Pages](https://flatdragon.github.io/vue-next-v-model-correct/).
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div>
+      <h1>Vue 2 v-model behaviour</h1>
+      <a href="https://github.com/flatdragon/vue-next-v-model-correct">See GitHub repo</a>
+    </div>
+
+    <div>
+      <h2>Using value with explicitly set 'undefined' value</h2>
+      <div>Input value: {{ value }}</div>
+      <input type="text" v-model="value">
+    </div>
+
+    <div>
+      <h2>Using some state object without defining 'value' property</h2>
+      <div>Input value: {{ state.value }}</div>
+      <input type="text" v-model="state.value">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data () {
+      return {
+        value: undefined,
+        state: {},
+      }
+    },
+  }
+</script>
